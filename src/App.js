bbfd4cb031d56931595cf1db5254c0a2d44b33c1
pri810 <111479652+pri810@users.noreply.github.com>
2025-01-27 20:33:56 +0530

    create new button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Form from "react-bootstrap/Form";
 import Btn from "./components/Btn";
 import Profile from "./components/Profile";
 import ProfileTable from "./components/ProfileTable";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [name, setName] = useState("")
@@ -16,6 +16,88 @@ function App() {
   const [link, setLink] = useState("")
 
   const [profiles, setProfiles] = useState([]);
+  const [singelProfile, setSingelProfile] = useState({
+    id: '',
+    link: '',
+    name: '',
+    desc: '',
+  });
+
+
+  useEffect(() => {
+    //get all profiles
+    fetch('http://localhost:8000/profiles')
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setProfiles(res)
+      })
+      .catch((error) => { console.log(error) })
+
+    // get single profile
+
+    // fetch('http://localhost:8000/profile/1')
+    // .then((res)=>{
+    //   return res.json();
+    // })
+    // .then((res)=>{
+    //   setSingelProfile(res)
+
+    // })
+    // .catch((error)=>{console.log(error)})
+  }, []);
+
+  function createprofile(e) {
+    e.preventDefault()
+    const id = profiles.length+1;
+    fetch("http://localhost:8000/profileCreate", {
+      method: "POST",
+      headers: { 'Content-Type': 'Application/json' },
+      body: JSON.stringify({
+        id: id,
+        name: name,
+        desc: desc,
+        link: link,
+
+      })
+    })
+      .then((res) => { return res.json() })
+      .then((res) => {
+        setProfiles(res);
+        setName("");
+        setDesc("");
+        setLink("");
+      })
+      .catch((error) => { console.log(error) })
+  }
+
+  function profileUpdate(e) {
+    e.preventDefault()
+    // const id =profiles.length+1;
+    fetch("http://localhost:8000/profileUpdate", {
+      method: "PUT",
+      headers: { 'Content-Type': 'Application/json' },
+      body: JSON.stringify({
+        id: singelProfile.id,
+        name: singelProfile.name,
+        desc: singelProfile.desc,
+        link: singelProfile.link,
+
+      })
+    })
+      .then((res) => { return res.json() })
+      .then((res) => {
+        setProfiles(res);
+        setSingelProfile({
+          id: '',
+          link: '',
+          name: '',
+          desc: '',
+        })
+      })
+      .catch((error) => { console.log(error) })
+  }
 
   // const profiles = [
   //   {
@@ -44,10 +126,69 @@ function App() {
   return (
     <div className="container mt-4">
       <h1>Profiles</h1>
+      <div className="row mb-3">
+        <div>
+          <div className="card">
+            {/* heading */}
+            <div className="card-header">
+              <h3>Update Profile</h3>
+            </div>
+            {/* body */}
+            <div className="card-body">
+              <Form>
+                {/* Name */}
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Name"
+                    onChange={(e) => { setSingelProfile({ ...singelProfile, name: e.target.value }) }} value={singelProfile.name} />
+                  {/* <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text> */}
+                </Form.Group>
+                {/* Description */}
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Description</Form.Label>
+                  <Form.Control type="text" placeholder="Enter Description"
+                    onChange={(e) => { setSingelProfile({ ...singelProfile, desc: e.target.value }) }} value={singelProfile.desc} />
+                  {/* <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text> */}
+                </Form.Group>
+                {/* Link */}
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Link</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter Github Profile Link"
+                    onChange={(e) => { setSingelProfile({ ...singelProfile, link: e.target.value }) }} value={singelProfile.link} />
+
+                  {/* <Form.Text className="text-muted">
+                    We'll never share your email with anyone else.
+                  </Form.Text> */}
+                </Form.Group>
+                {/* Submit Button */}
+                <Button variant="primary" type="submit" onClick={(e) => {
+                  profileUpdate(e)
+                  // e.preventDefault()
+                  // setProfiles((old)=>[{
+                  //   name: name,
+                  //   desc: desc,
+                  //   link: link,
+                  // },
+                  //   ...old 
+                  // ])
+                }}>
+                  edit Profile
+                </Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="row">
         {/* Profile List */}
         <div className="col-md-8">
-          <ProfileTable profiles={profiles} />
+          <ProfileTable profiles={profiles} set={setSingelProfile} setprofile={setProfiles} />
         </div>
         {/* Create Profile Form */}
         <div className="col-md-4">
@@ -62,7 +203,8 @@ function App() {
                 {/* Name */}
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Name</Form.Label>
-                  <Form.Control type="text" placeholder="Enter Name" onChange={(e)=>{setName(e.target.value)}}/>
+                  <Form.Control type="text" placeholder="Enter Name"
+                    onChange={(e) => { setName(e.target.value) }} value={name} />
                   {/* <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                   </Form.Text> */}
@@ -70,7 +212,8 @@ function App() {
                 {/* Description */}
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                   <Form.Label>Description</Form.Label>
-                  <Form.Control type="text" placeholder="Enter Description" onChange={(e)=>{setDesc(e.target.value)}}/>
+                  <Form.Control type="text" placeholder="Enter Description"
+                    onChange={(e) => { setDesc(e.target.value) }} value={desc} />
                   {/* <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                   </Form.Text> */}
@@ -81,22 +224,15 @@ function App() {
                   <Form.Control
                     type="text"
                     placeholder="Enter Github Profile Link"
-                    // onChange={(e)=>{setLink(e.target.value)}}
-                  />
+                    onChange={(e) => { setLink(e.target.value) }} value={link} />
+
                   {/* <Form.Text className="text-muted">
                     We'll never share your email with anyone else.
                   </Form.Text> */}
                 </Form.Group>
                 {/* Submit Button */}
-                <Button variant="primary" type="submit" onClick={(e)=>{
-                  e.preventDefault()
-                  setProfiles((old)=>[{
-                    name: name,
-                    desc: desc,
-                    link: link,
-                  },
-                    ...old 
-                  ])
+                <Button variant="primary" type="submit" onClick={(e) => {
+                  createprofile(e)
                 }}>
                   Add Profile
                 </Button>
@@ -104,6 +240,7 @@ function App() {
             </div>
           </div>
         </div>
+
       </div>
       {/* <div className='row'>
         {
--- src/components/ProfileTable.js
+++ src/components/ProfileTable.js
@@ -2,7 +2,7 @@
 import Table from "react-bootstrap/Table";
 import Button from "react-bootstrap/Button";
 
-function ProfileTable({ profiles }) {
+function ProfileTable({ profiles,set,setprofile}) {
   return (
     <Table striped bordered hover>
       <thead>
@@ -11,12 +11,13 @@ function ProfileTable({ profiles }) {
           <th>Name</th>
           <th>Description</th>
           <th>Github Profile Link</th>
+          <th>Actions</th>
         </tr>
       </thead>
       <tbody>
         {profiles.map((profile,index) => {
           return (
-            <tr>
+            <tr key={index}>
               <td>{index+1}</td>
               <td>{profile.name}</td>
               <td>{profile.desc}</td>
@@ -24,6 +25,36 @@ function ProfileTable({ profiles }) {
                 <a href={profile.link} target="_" variant="primary">
                   <Button>Github Profile</Button>
                 </a>
+              </td>
+              <td>
+                  <Button  className="m-1"
+                  onClick={()=>{
+                    fetch('http://localhost:8000/profiles/' +profile.id)
+                    .then((res)=>{
+                      return res.json();
+                    })
+                    .then((res)=>{
+                      set(res)
+                      
+                    })
+                    .catch((error)=>{console.log(error)})
+                  }}
+                  >Edit </Button>
+                   <Button 
+                  onClick={()=>{
+                    fetch('http://localhost:8000/profileDelete/' +profile.id,{
+                      method:'DELETE'
+                    })
+                    .then((res)=>{
+                      return res.json();
+                    })
+                    .then((res)=>{
+                      setprofile(res)
+                      
+                    })
+                    .catch((error)=>{console.log(error)})
+                  }}
+                  >Delete </Button>
               </td>
             </tr>
           );
